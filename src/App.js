c39e7f40d7cce6f562471a17e3973e6924d3c313
Aygün Bayır <turkoaygun@gmail.com>
2023-10-20 14:27:26 +0300

    fix: lazy import suspense fallback

--- src/App.js
+++ src/App.js
@@ -14,19 +14,19 @@ function App() {
   return (
     <div className="flex min-h-screen flex-col bg-gradient-to-tl from-gray-100 to-white">
       <Router>
-        <Suspense>
-          <ScrollToTop />
-          <Header />
-          <main className="container mx-auto mb-12 flex-1 space-y-4 px-1 py-1 font-inter text-gray-700 max-xl:w-96">
+        <ScrollToTop />
+        <Header />
+        <main className="container mx-auto mb-12 flex-1 space-y-4 px-1 py-1 font-inter text-gray-700 max-xl:w-96">
+          <Suspense fallback={<div>Loading...</div>}>
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/product/:productId" element={<ProductDetail />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </main>
-          <Footer />
-        </Suspense>
+          </Suspense>
+        </main>
+        <Footer />
       </Router>
     </div>
   );
